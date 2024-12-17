8a25a37b0fd1b0bae68fb50769e037865bec21c4
Andor <david-andor@kozpontiagy.hu>
2024-12-17 22:13:41 +0100

    Back to pcov in CircleCI

--- tests/acceptance/Task/VersionTaskCest.php
+++ tests/acceptance/Task/VersionTaskCest.php
@@ -24,7 +24,7 @@ class VersionTaskCest
         $tester->assertSame(0, $exitCode, 'exitCode');
 
         $tester->assertStringContainsString(
-            "The version of the PHPStan is: '1.12.12'",
+            "The version of the PHPStan is: '1.12.13'",
             $stdOutput,
             'stdOutput contains',
         );
