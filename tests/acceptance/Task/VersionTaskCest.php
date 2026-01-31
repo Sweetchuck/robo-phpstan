2fd76a611a5cf66cfbb05c2d7d13944e7dc3e787
Andor <david-andor@kozpontiagy.hu>
2026-01-31 15:29:51 +0100

    Upgrade dependencies

--- tests/acceptance/Task/VersionTaskCest.php
+++ tests/acceptance/Task/VersionTaskCest.php
@@ -24,7 +24,7 @@ class VersionTaskCest
         $tester->assertSame(0, $exitCode, 'exitCode');
 
         $tester->assertStringContainsString(
-            "The version of the PHPStan is: '1.12.13'",
+            "The version of the PHPStan is: '1.12.32'",
             $stdOutput,
             'stdOutput contains',
         );
